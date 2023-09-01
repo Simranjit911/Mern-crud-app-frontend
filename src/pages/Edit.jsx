d2251434bca669d63aa3a34e76965bbee409f1a2
Simranjit911 <simranjit94938@gmail.com>
2023-09-01 12:51:26 +0530

    validation

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -60,7 +60,7 @@ const Edit = () => {
           value={input.age}
           onChange={(e) =>
             {
-              if(e.target.value<=1)return toast.error("Enter Valid Age")
+              if(e.target.value<=-1)return toast.error("Enter Valid Age")
 
               setinput({ ...input, [e.target.name]: e.target.value })
             }
